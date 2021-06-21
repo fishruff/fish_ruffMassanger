e9154fef56c91b6ef1ae6ba8f47421fba358aaf4
Egor Kolesnikov <freethinkel@gmail.com>
2021-06-21 14:15:11 +0300

    fix state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
+import './App.css';
 //components
-import ContentProfile from "./components/Content/Profile/ContentProfile";
-import ContentMessages from "./components/Content/Messager/ContentMessages";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import ContentProfile from './components/Content/Profile/ContentProfile';
+import ContentMessages from './components/Content/Messager/ContentMessages';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = (props) => {
-  return (
-    <BrowserRouter>
-      <div className="App-wrapper">
-        <Header />
-        <Navbar />
-        <ContentMessages />
-        <Route
-          path="/messages"
-          render={() => <ContentMessages state={props.state} />}
-        />
-        <Route path="/profile" render={() => <ContentProfile />} />
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App-wrapper'>
+				<Header />
+				<Navbar />
+				<ContentMessages state={props.state} />
+				<Route
+					path='/messages'
+					render={() => <ContentMessages state={props.state} />}
+				/>
+				<Route path='/profile' render={() => <ContentProfile />} />
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- src/components/Content/Messager/ContentMessages.jsx
+++ src/components/Content/Messager/ContentMessages.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 //import state from "../../../state";
-import "./ContentMessages.css";
-import Dialogs from "./Dialogs";
-import MessageUser from "./MessageUser";
+import './ContentMessages.css';
+import Dialogs from './Dialogs';
+import MessageUser from './MessageUser';
 
 const ContentMessages = (props) => {
-  let UsersInfoElement = props.state.MassagePage.UsersInfo.map((u) => (
-    <MessageUser UserName={u.UserName} id={u.id} LMessage={u.LMessage} />
-  ));
+	let UsersInfoElement = props.state.MassagePage.UsersInfo.map((u) => (
+		<MessageUser UserName={u.UserName} id={u.id} LMessage={u.LMessage} />
+	));
 
-  return (
-    <div className="content">
-      <div className="Messages">
-        <div className="users">{UsersInfoElement}</div>
-        <div className="dialogs">
-          <Dialogs />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='content'>
+			<div className='Messages'>
+				<div className='users'>{UsersInfoElement}</div>
+				<div className='dialogs'>
+					<Dialogs />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ContentMessages;
